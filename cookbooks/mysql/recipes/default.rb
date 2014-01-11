06eb9198eba92e58f8c090e60b8ffe4567b5b7e1
Citlalli Murillo <citmusa@hotmail.com>
2014-01-11 11:40:40 -0600

    FIX: Access denied for user root@localhost

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -1,4 +1,3 @@
-
 %w{mysql-server mysql-client libmysqlclient-dev python-mysqldb}.each do |pkg|
     package pkg do
         action :install
@@ -7,6 +6,7 @@ end
 
 if node.has_key?("mysql")
    execute "Set MySql root password" do
-       command "mysqladmin -u root password " + node[:mysql][:root_password]
+       #command "mysqladmin -u root password " + node[:mysql][:root_password]
+       command "mysql -u root -p; UPDATE mysql.user SET Password=PASSWORD('"+node[:mysql][:root_password]+"') WHERE User='root'; flush privileges;"
    end
 end
